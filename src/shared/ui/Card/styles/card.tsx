cd0e76542d3e052b63dd459f4944b2f7517c5e07
dowlsss <dowlssss@yandex.by>
2023-08-12 21:35:21 +0500

    some card changes

--- src/shared/ui/Card/styles/card.tsx
+++ src/shared/ui/Card/styles/card.tsx
@@ -13,7 +13,11 @@ const {Title, Text} = Typography;
 export const Card = ({date, place, tags, title, imgLink}: Card) => {
     return (
         <CardStyled>
-            <CardCover img={imgLink}/>
+            <CardTop>
+                <CardImage>
+                    <img src={imgLink}/>
+                </CardImage>
+            </CardTop>
             <CardBody>
                 <Title style={{margin: 0}} level={5}>{title}</Title>
                 <Text>{date}</Text>
@@ -21,6 +25,7 @@ export const Card = ({date, place, tags, title, imgLink}: Card) => {
                 <Space size={[1, 1]} wrap>
                     {tags.map((tag) => (
                         <Tag
+                            bordered={false}
                             key={tag}>
                             {tag}
                         </Tag>
@@ -34,30 +39,37 @@ export const Card = ({date, place, tags, title, imgLink}: Card) => {
 const CardStyled = styled('div')`
   display: flex;
   width: 240px;
+  position: relative;
   flex-direction: column;
-  align-items: flex-start;
   border-radius: 2px;
   border: 1px solid #F5F5F5;
   background: white;
-`
+`;
 const CardBody = styled('div')`
   display: flex;
-  padding: 24px;
   flex-direction: column;
+  flex: 1 0 auto; 
+  padding: 24px;
   gap: 8px;
-`
+`;
 
-const CardCover = styled('div')<{ img: string }>`
-  display: flex;
-  width: 240px;
-  height: 134px;
-  background-image: url('${props => props.img}');
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-attachment: fixed;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-  align-items: flex-start;
-`
+const CardImage = styled('div')`
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  > img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const CardTop = styled('div')`
+  flex: 0 0 134px;
+  position: relative;
+  overflow: hidden;
+`;
