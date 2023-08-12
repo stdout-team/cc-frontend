03148d6fcae47fb74f65b2804f161deb54767a75
gaskeo <zeytunyan2004@mail.ru>
2023-08-12 20:12:01 +0500

    feat(map): add demo map page

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import HeaderWidget from "@/widgets/HeaderWidget";
 import FooterWidget from "@/widgets/FooterWidget";
-import {Row} from "antd";
+import {Col, Row} from "antd";
 import {Map} from '@/shared/ui';
 
 export default function Home() {
@@ -14,8 +14,11 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
             <HeaderWidget/>
-            <Row align="middle">
-                <Map gisKey='042b5b75-f847-4f2a-b695-b5f58adc9dfd' width="100%" height="500px" popups={[]} id='main' center={[60.613589, 56.843085]} zoom={13}/>
+            <Row align="middle" justify="center">
+                <Col span={17}>
+                    <Map gisKey='042b5b75-f847-4f2a-b695-b5f58adc9dfd' width="100%" height="500px" popups={[]} id='main'
+                         center={[60.613589, 56.843085]} zoom={13}/>
+                </Col>
             </Row>
             <FooterWidget/>
         </>
